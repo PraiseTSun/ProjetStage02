792dd1c6df9f13f0bb98ceba7ed8e81654268590
Samir Badi <samirbadi1@outlook.com>
2022-09-07 17:40:53 -0400

    EQ2-19 Create the interface CompanyRepository

--- src/main/java/projet/projetstage02/service/CompanyService.java
+++ src/main/java/projet/projetstage02/service/CompanyService.java
@@ -3,11 +3,18 @@ package projet.projetstage02.service;
 import org.springframework.stereotype.Component;
 import projet.projetstage02.modele.AbstractUser;
 import projet.projetstage02.modele.Company;
+import projet.projetstage02.repository.CompanyRepository;
 
 @Component
 public class CompanyService {
-    public Company createCompany(String firstName, String lastName, String name, String email, String password, AbstractUser.Department departement) {
-        Company company = new Company(firstName, lastName, email, password, departement, name);
+    private CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public Company createCompany(String firstName, String lastName, String name, String email, String password, AbstractUser.Department department) {
+        Company company = new Company(firstName, lastName, email, password, department, name);
         return null;
     }
 }
