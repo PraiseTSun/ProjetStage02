fb02fe24cfa498e2e4480c391a8ca9f169571435
Samir Badi <samirbadi1@outlook.com>
2022-09-07 14:47:11 -0400

    Create class Compagny taht extend on AbstractUser

---
+++ src/main/java/projet/projetstage02/modele/Compagny.java
@@ -0,0 +1,20 @@
+package projet.projetstage02.modele;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Compagny extends AbstractUser{    
+    private  Department department;
+
+    public Compagny(String firstName, String lastName, String email, String password, Department department) {
+        super(firstName, lastName, email, password);
+        this.department = department;
+    }
+}
