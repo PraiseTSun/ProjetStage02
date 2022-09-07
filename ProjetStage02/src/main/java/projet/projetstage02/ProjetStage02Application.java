b68c10bd6f3fc161139e595470960c5bc41a5f75
Samir Badi <samirbadi1@outlook.com>
2022-09-07 11:07:47 -0400

    EQ2-32 Setup Samir

---
+++ ProjetStage02/src/main/java/projet/projetstage02/ProjetStage02Application.java
@@ -0,0 +1,13 @@
+package projet.projetstage02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetStage02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetStage02Application.class, args);
+    }
+
+}
---
+++ ProjetStage02/src/test/java/projet/projetstage02/ProjetStage02ApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.projetstage02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetStage02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
