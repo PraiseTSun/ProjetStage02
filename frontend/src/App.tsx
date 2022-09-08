40326afb9305399aef5bb84b8312af0422ac4da4
Yan Zhou <zhouyan87910@gmail.com>
2022-09-07 22:11:29 -0400

    EQ2-33 finir de créer une page inscription

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import PageInscription from "./components/PageInscription";
 
 function App() {
+  const [utilisateurs, setUtilisateurs] = useState([])
+
+  const onInscrire = async  (compte : object) => {
+    const res = await fetch('http://localhost:8080/utilisateurs',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(compte)
+        })
+      const data = await res.json();
+      utilisateurs.join(data)
+  }
   return (
-    <PageInscription/>
+    <PageInscription onInscrire={onInscrire}/>
   );
 }
 
--- frontend/src/components/PageInscription.js
+++ frontend/src/components/PageInscription.js
@@ -1,19 +1,56 @@
 import {useState} from "react";
 
-const PageInscription = ({}) => {
+const PageInscription = ({onInscrire}) => {
+
     const [typePersonne, setTypePersonne] = useState('')
     const [departement, setDepartement] = useState('')
     const [courriel, setCourriel] = useState('')
     const [motDePasse, setMotDePasse] = useState('')
     const [validationMotDePasse, setValidationMotDePasse] = useState('')
+    const [courrielErr, setCourrielErr] = useState(false)
+
+    const validEmail = new RegExp(
+        '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+    );
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!typePersonne || !departement || !courriel || !motDePasse || !validationMotDePasse) {
+            alert('Veuillez ajouter toutes les informations')
+            return
+        }
+
+        if (motDePasse !== validationMotDePasse) {
+            alert('Vérifier le mot de passe et le mot de passe ne correspondent pas')
+            return
+        }
+        if(motDePasse.length < 8){
+            alert("La longueur du mot de passe est supérieure à 7")
+            return
+        }
+        if(!validEmail.test(courriel)){
+            setCourrielErr(true)
+            return;
+        }
+
+        onInscrire({typePersonne : typePersonne, departement : departement, courriel : courriel, motDePasse : motDePasse})
+        setTypePersonne('')
+        setDepartement('')
+        setCourriel('')
+        setMotDePasse('')
+        setValidationMotDePasse('')
+        setCourrielErr(false)
+    }
     return(
         <div>
             <div>Inscription</div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div>
                     <label>
                         Type de personne :
-                        <select   onChange={(e) => setTypePersonne(e.target.value)}>
+                        <select defaultValue={'Choix un type'} onChange={(e) => setTypePersonne(e.target.value)}>
+                            <option value="Choix un type" disabled>Choix un type</option>
                             <option value="Etudiant">Etudiant</option>
                             <option value="Entreprise">Entreprise</option>
                         </select>
@@ -21,8 +58,9 @@ const PageInscription = ({}) => {
                 </div>
                <div>
                    <label>
-                       Departement :
-                       <select  onChange={(e) => setDepartement(e.target.value)}>
+                       Département :
+                       <select defaultValue={'Choix un type'} onChange={(e) => setDepartement(e.target.value)}>
+                           <option value="Choix un type" disabled>Choix un département</option>
                            <option value="Techniques de l’informatique">Techniques de l’informatique</option>
                            <option value="Techniques de la logistique du transport">Techniques de la logistique du transport</option>
                        </select>
@@ -30,11 +68,13 @@ const PageInscription = ({}) => {
                </div>
 
                 <div className='form-control'>
-                    <label>Nom</label>
-                    <input type='text' placeholder='Courriel'
+                    <label>Courriel</label>
+                    <input type='email' placeholder='Courriel'
                            value={courriel}
-                           onChange={(e) => setCourriel(e.target.value)}/>
+                           onChange={(e) => setCourriel(e.target.value)}
+                           required/>
                 </div>
+                {courrielErr && <p>Your email is invalid</p>}
                 <div className='form-control'>
                     <label>Mot de pass</label>
                     <input type='text' placeholder='Mot de passe'
@@ -47,6 +87,7 @@ const PageInscription = ({}) => {
                            value={validationMotDePasse}
                            onChange={(e) => setValidationMotDePasse(e.target.value)}/>
                 </div>
+                <input type='submit' value='Inscrire' />
             </form>
         </div>
 
