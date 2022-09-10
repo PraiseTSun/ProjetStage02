1b1b8bd144e128fd74a8df2325ebae9435a25890
Samir Badi <Samirbadi1@outlook.com>
2022-09-10 10:35:29 -0400

    EQ2-19 Create a DTO for the AbstractUser

--- src/main/java/projet/projetstage02/DTO/AbstractUserDTO.java
+++ src/main/java/projet/projetstage02/DTO/AbstractUserDTO.java
@@ -1,5 +1,17 @@
 package projet.projetstage02.DTO;
 
-public class AbstractUserDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AbstractUserDTO {
+    protected String id;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String password;
+    protected Boolean isConfirmed;
 }
