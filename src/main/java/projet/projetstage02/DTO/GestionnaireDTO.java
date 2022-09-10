1021d3f99d83527324809f7e162e85ae978a7509
Samir Badi <Samirbadi1@outlook.com>
2022-09-10 10:55:49 -0400

    EQ2-19 Create a DTO for the Gestionnaire

---
+++ src/main/java/projet/projetstage02/DTO/GestionnaireDTO.java
@@ -0,0 +1,25 @@
+package projet.projetstage02.DTO;
+
+import projet.projetstage02.modele.Gestionnaire;
+
+public class GestionnaireDTO extends AbstractUserDTO<Gestionnaire> {
+
+    public GestionnaireDTO(Gestionnaire gestionnaire){
+            firstName = gestionnaire.getFirstName();
+            lastName = gestionnaire.getLastName();
+            email = gestionnaire.getEmail();
+            password = gestionnaire.getPassword();
+    }
+
+    @Override
+    public Gestionnaire GetClass() {
+        Gestionnaire gestionnaire = new Gestionnaire(
+                firstName,
+                lastName,
+                email,
+                password
+        );
+        gestionnaire.setId(Long.parseLong(id));
+        return gestionnaire;
+    }
+}
