fd2ae8f239a7ce2cfec98404b909d7c6702b60ec
Mathieu Ford <mathieu.ford@gmail.com>
2022-09-12 18:20:38 -0400

    Added register link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,10 @@ function App() {
   return (
     <Container>
       <Row className="vh-100">
-        <Col m-auto className="m-auto col-4">
+        <Col m-auto className="m-auto col-4 text-center">
           <h1 className="text-warning fw-bold text-center">OSE KILLER</h1>
           <LoginPage />
+          <a href="/register" className="link-warning">Nouveau utilisateur? Inscrivez vous ici.</a>
         </Col>
       </Row>
     </Container>
