b3e5156e3bc79d14dab540fae4c29969a710c065
Mathieu Ford <mathieu.ford@gmail.com>
2022-09-13 11:15:10 -0400

    Form with no endpoint request

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Form, Row, Col } from "react-bootstrap";
 
-function LoginForm() {
+const LoginForm = (props: { setUser: Function }): JSX.Element => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isInvalidLoggin, setIsInvalidLoggin] = useState("");
+
+    const onSubmit = (event: React.SyntheticEvent): void => {
+        event.preventDefault
+        //TODO
+    }
+
     return (
-        <Form>
+        <Form onSubmit={onSubmit}>
             <Form.Group>
                 <Form.Label className="text-white fw-bold h5">Adresse courriel</Form.Label>
-                <Form.Control type="email" required></Form.Control>
+                <Form.Control type="email" required value={email} onChange={field => setEmail(field.target.value)}></Form.Control>
             </Form.Group>
             <Form.Group>
                 <Form.Label className="text-white fw-bold mt-2 h5">Mot de passe</Form.Label>
-                <Form.Control type="password" required></Form.Control>
+                <Form.Control type="password" required value={password} onChange={field => setPassword(field.target.value)}></Form.Control>
             </Form.Group>
+            {isInvalidLoggin ? <h5 className="text-danger fw-bold text-center mt-2">Courriel ou mot de passe invalide!</h5> : <></>}
             <Row className="mt-3">
                 <Col className="text-center">
-                    <Button className="btn-success mx-auto w-75" type="submit">
-                        Connecter
-                    </Button>
+                    <Form.Control type="submit" value="Connecter" className="btn btn-success mx-auto w-75"></Form.Control>
                 </Col>
             </Row>
         </Form>
