6b2fb5ede194f8124f7a60903bd18cfe2a047d81
Mathieu Ford <mathieu.ford@gmail.com>
2022-09-14 11:36:33 -0400

    Added Radio buttons to login form

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,31 +1,43 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Form, Row, Col } from "react-bootstrap";
+import { Form, Row, Col, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
 const LoginForm = (props: { setUser: Function }): JSX.Element => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [userType, setUserType] = useState("student")
     const [isInvalidLoggin, setIsInvalidLoggin] = useState("");
 
     const onSubmit = (event: React.SyntheticEvent): void => {
-        event.preventDefault
-        //TODO
+        event.preventDefault();
+        setEmail("");
+        setPassword("");
+        setUserType("student");
     }
 
     return (
         <Form onSubmit={onSubmit}>
-            <Form.Group>
-                <Form.Label className="text-white fw-bold h5">Adresse courriel</Form.Label>
-                <Form.Control type="email" required value={email} onChange={field => setEmail(field.target.value)}></Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label className="text-white fw-bold mt-2 h5">Mot de passe</Form.Label>
-                <Form.Control type="password" required value={password} onChange={field => setPassword(field.target.value)}></Form.Control>
-            </Form.Group>
-            {isInvalidLoggin ? <h5 className="text-danger fw-bold text-center mt-2">Courriel ou mot de passe invalide!</h5> : <></>}
-            <Row className="mt-3">
-                <Col className="text-center">
-                    <Form.Control type="submit" value="Connecter" className="btn btn-success mx-auto w-75"></Form.Control>
+            <ToggleButtonGroup className="d-flex" name="userType" type="radio" value={userType} onChange={field => setUserType(field)}>
+                <ToggleButton className="w-100" id="1" variant="info" value="student">Étudiant</ToggleButton>
+                <ToggleButton className="w-100" id="2" variant="info" value="company">Entreprise</ToggleButton>
+                <ToggleButton className="w-100" id="3" variant="info" value="manager">Gestionnaire</ToggleButton>
+            </ToggleButtonGroup>
+            <Row>
+                <Col className="px-4 pb-2 pt-1">
+                    <Form.Group>
+                        <Form.Label className="fw-bold h5">Adresse courriel</Form.Label>
+                        <Form.Control type="email" required value={email} onChange={field => setEmail(field.target.value)}></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label className="fw-bold mt-2 h5">Mot de passe</Form.Label>
+                        <Form.Control type="password" required value={password} onChange={field => setPassword(field.target.value)}></Form.Control>
+                    </Form.Group>
+                    {isInvalidLoggin ? <h5 className="text-danger fw-bold text-center mt-2">Courriel ou mot de passe invalide!</h5> : <></>}
+                    <Row className="mt-3">
+                        <Col className="text-center">
+                            <Form.Control type="submit" value="Connecter" className="btn btn-success mx-auto w-75"></Form.Control>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Form>
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -6,16 +6,20 @@ import LoginForm from "../components/LoginForm";
 const LoginPage = (props: { setUser: Function }): JSX.Element => {
     const [isLogginPage, setIsLogginPage] = useState(true)
 
-    return (    
+    return (
         <Row className="vh-100">
             <Col className="m-auto col-lg-4 col-md-6">
                 <h1 className="text-warning fw-bold text-center display-3">OSE KILLER</h1>
-                {isLogginPage ? <LoginForm setUser={props.setUser} /> : <h1>Mettre register form ici</h1>}
                 <Row>
-                    <Col className="text-center">
-                        <a href="#" className="link-warning" onClick={() => setIsLogginPage(!isLogginPage)}>
-                            {isLogginPage ? "Nouveau utilisateur? Inscrivez vous ici." : "Retour à la page de connexion"}
-                        </a>
+                    <Col className="bg-white rounded p-0">
+                        {isLogginPage ? <LoginForm setUser={props.setUser} /> : <h1>Mettre register form ici</h1>}
+                        <Row>
+                            <Col className="text-center mb-2">
+                                <a href="#" className="link-warning" onClick={() => setIsLogginPage(!isLogginPage)}>
+                                    {isLogginPage ? "Nouveau utilisateur? Inscrivez vous ici." : "Retour à la page de connexion"}
+                                </a>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Col>
