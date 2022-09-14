d15aa405bb9ba47d8ee194668b2e2f8d7a2d4df0
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-09-14 16:38:42 -0400

    Changed create user endpoint to separate it according to the user

--- src/main/java/projet/projetstage02/controller/RootController.java
+++ src/main/java/projet/projetstage02/controller/RootController.java
@@ -23,28 +23,31 @@ public class RootController {
     GestionnaireService gestionnaireService;
 
 
-    private ResponseEntity<String> createStudent(StudentDTO studentDTO){
+    @PostMapping("/createStudent")
+    public ResponseEntity<String> createStudent(@RequestBody StudentDTO studentDTO){
         if(!studentService.isUniqueEmail(studentDTO.getEmail())){
             ResponseEntity.status(HttpStatus.CONFLICT).body("Cette adresse email est déjà utilisée.");
         }
         studentService.createStudent(studentDTO);
+        studentService.sendConfirmationMail(studentDTO.getOrigin());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
-    private ResponseEntity<String> createCompany(CompanyDTO companyDTO){
+    @PostMapping("/createCompany")
+    public ResponseEntity<String> createCompany(@RequestBody CompanyDTO companyDTO){
         if(!companyService.isUniqueEmail(companyDTO.getEmail())){
             ResponseEntity.status(HttpStatus.CONFLICT).body("Cette adresse email est déjà utilisée.");
         }
         companyService.createCompany(companyDTO);
+        companyService.sendConfirmationMail(companyDTO.getOrigin());
         return ResponseEntity.status(HttpStatus.CREATED).build();
-
     }
-
-    private ResponseEntity<String> createGestionnaire(GestionnaireDTO gestionnaireDTO){
+    @PostMapping("/createGestionaire")
+    public ResponseEntity<String> createGestionnaire(@RequestBody GestionnaireDTO gestionnaireDTO){
         if(!gestionnaireService.isUniqueEmail(gestionnaireDTO.getEmail())){
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Cette adresse email est déjà utilisée.");
         }
         gestionnaireService.createGestionnaire(gestionnaireDTO);
+        gestionnaireService.sendConfirmationMail(gestionnaireDTO.getOrigin());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
