a534650b79afe9a83f6da3eb8f75e17c6cbbf5f2
Samir Badi <Samirbadi1@outlook.com>
2022-09-17 10:30:52 -0400

    EQ2-49 Add radio button for the different user

--- frontend/src/pages/UserValidation.tsx
+++ frontend/src/pages/UserValidation.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-import {Row} from "react-bootstrap";
+import {Container, Col, Row, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
 const UserValidation = () => {
     return (
-        <Row>hello word</Row>
+        <Container fluid>
+            <Col xs={1}>
+                <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+                    <ToggleButton id="StudentValid" value={1}>
+                        Student
+                    </ToggleButton>
+                    <ToggleButton id="CompanyValid" value={2}>
+                        Company
+                    </ToggleButton>
+                    <ToggleButton id="GestionnairValid" value={3}>
+                        Gestionnaire
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            </Col>
+            <Col xs={11}>
+
+            </Col>
+        </Container>
     );
 }
 
