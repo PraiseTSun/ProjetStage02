7b5cefbd7583544b2225ff7a211d3a2dbeb69f1a
Samir Badi <Samirbadi1@outlook.com>
2022-09-17 11:29:31 -0400

    EQ2-49 Create component ValidationCompany

---
+++ frontend/src/components/ValidationGetionnaire.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Col} from "react-bootstrap";
+
+const ValidationGetionnaire = () => {
+    return (
+        <Col>Gestionnaire</Col>
+    );
+}
+
+export default ValidationGetionnaire;
