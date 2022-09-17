9d8f8a84154a1a6fd127daf1f73861df299abe54
Samir Badi <Samirbadi1@outlook.com>
2022-09-17 11:27:56 -0400

    EQ2-49 Create component ValidationCompany

---
+++ frontend/src/components/ValidationCompany.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Col} from "react-bootstrap";
+
+const ValidationCompany = () => {
+    return(
+        <Col>Company</Col>
+    );
+}
+
+export default ValidationCompany;
--- frontend/src/components/ValidationStudent.tsx
+++ frontend/src/components/ValidationStudent.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import {Col} from "react-bootstrap";
 
 const ValidationStudent = () => {
-
+    return(
+        <Col>Student</Col>
+    );
 }
 
 export default ValidationStudent;
