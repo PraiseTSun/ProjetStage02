1739395e1a7cf5b1f802d8d2b83d94fa7cea360b
Mathieu Ford <mathieu.ford@gmail.com>
2022-09-26 17:03:17 -0400

    EQ2-101 : Added OffreDTO constructor with Offre parameter

--- src/main/java/projet/projetstage02/DTO/OffreDTO.java
+++ src/main/java/projet/projetstage02/DTO/OffreDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import projet.projetstage02.model.Offre;
 
 @Data
 @NoArgsConstructor
@@ -17,4 +18,13 @@ public class OffreDTO {
     private int heureParSemaine;
     private String adresse;
     private byte[] pdf;
+
+    public OffreDTO(Offre offre) {
+        this.nomDeCompagnie = offre.getNomDeCompagie();
+        this.department = offre.getDepartment().departement;
+        this.position = offre.getPosition();
+        this.heureParSemaine = offre.getHeureParSemaine();
+        this.adresse = offre.getAdresse();
+        this.pdf = offre.getPdf();
+    }
 }
