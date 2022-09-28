1231064a077b360badb8b8f20b17146c468138d7
Mathieu Ford <mathieu.ford@gmail.com>
2022-09-28 08:52:03 -0400

    EQ2-101 Refactor

--- src/test/java/projet/projetstage02/ProjetStage02ApplicationTests.java
+++ src/test/java/projet/projetstage02/ProjetStage02ApplicationTests.java
@@ -1,13 +1,8 @@
 package projet.projetstage02;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjetStage02ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
