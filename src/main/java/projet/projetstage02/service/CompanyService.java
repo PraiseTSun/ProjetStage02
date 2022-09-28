e14f9e0e5792b0e649b533c34e163cf791198fa9
Mathieu Ford <mathieu.ford@gmail.com>
2022-09-28 08:52:29 -0400

    EQ2-101 Fixed bug where offreDTO got department toString() and not value

--- src/main/java/projet/projetstage02/service/CompanyService.java
+++ src/main/java/projet/projetstage02/service/CompanyService.java
@@ -19,15 +19,22 @@ public class CompanyService {
     private final CompanyRepository companyRepository;
     private final OffreRepository offreRepository;
 
-    public long createOffre(OffreDTO offreDTO){
-        Offre offre = new Offre(offreDTO.getNomDeCompagnie(), Department.getDepartment(offreDTO.getDepartment()), offreDTO.getPosition(),
-                offreDTO.getHeureParSemaine(), offreDTO.getAdresse(), offreDTO.getPdf());
+    public long createOffre(OffreDTO offreDTO) {
+        Offre offre = Offre.builder()
+                .nomDeCompagie(offreDTO.getNomDeCompagnie())
+                .department(Department.getDepartment(offreDTO.getDepartment()))
+                .position(offreDTO.getPosition())
+                .heureParSemaine(offreDTO.getHeureParSemaine())
+                .adresse(offreDTO.getAdresse())
+                .pdf(offreDTO.getPdf())
+                .build();
+
         offre.setId(Long.parseLong(offreDTO.getId()));
         return offreRepository.save(offre).getId();
     }
 
     public void saveCompany(String firstName, String lastName, String name, String email, String password,
-                            Department department) {
+            Department department) {
         CompanyDTO dto = CompanyDTO.builder()
                 .firstName(firstName)
                 .lastName(lastName)
@@ -52,13 +59,15 @@ public class CompanyService {
 
     public CompanyDTO getCompanyById(Long id) throws NonExistentUserException {
         var companyOpt = companyRepository.findById(id);
-        if (companyOpt.isEmpty()) throw new NonExistentUserException();
+        if (companyOpt.isEmpty())
+            throw new NonExistentUserException();
         return new CompanyDTO(companyOpt.get());
     }
 
     public CompanyDTO getCompanyByEmailPassword(String email, String password) throws NonExistentUserException {
         var companyOpt = companyRepository.findByEmailAndPassword(email.toLowerCase(), password);
-        if (companyOpt.isEmpty()) throw new NonExistentUserException();
+        if (companyOpt.isEmpty())
+            throw new NonExistentUserException();
         return new CompanyDTO(companyOpt.get());
     }
 }
