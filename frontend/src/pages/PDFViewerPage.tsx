80d09aa4500a58e5c24ffc682eaa1ce35c72a487
Mathieu Ford <mathieu.ford@gmail.com>
2022-10-03 17:24:26 -0400

    EQ2-80 Added PDFViewerPage

---
+++ frontend/src/pages/PDFViewerPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const PDFViewerPage = (): JSX.Element => {
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default PDFViewerPage;
