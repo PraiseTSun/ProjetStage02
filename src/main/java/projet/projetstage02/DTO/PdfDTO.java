c8e088c46dc3a9e5107c6db3d8987b5a00b86ff0
Mathieu Ford <mathieu.ford@gmail.com>
2022-10-03 11:38:27 -0400

    EQ2-77 Changed  /unvalidatedCvStudents and added /studentCv/{studentId}

--- src/main/java/projet/projetstage02/DTO/PdfDTO.java
+++ src/main/java/projet/projetstage02/DTO/PdfDTO.java
@@ -2,7 +2,6 @@ package projet.projetstage02.DTO;
 
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/test/java/projet/projetstage02/controller/RootControllerTest.java
+++ src/test/java/projet/projetstage02/controller/RootControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import projet.projetstage02.DTO.*;
 import projet.projetstage02.exception.NonExistentOfferExeption;
