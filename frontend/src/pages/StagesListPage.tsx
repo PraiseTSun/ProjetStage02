5b1e08cbecd288b40d39a97d2b806c2355178394
Mathieu Ford <mathieu.ford@gmail.com>
2022-10-05 16:07:16 -0400

    EQ2-90 Created StagesListPage

---
+++ frontend/src/pages/StagesListPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const StagesListPage = (): JSX.Element => {
+
+
+    return (
+        <h1>
+            HELLO
+        </h1>
+    );
+}
+
+export default StagesListPage;
