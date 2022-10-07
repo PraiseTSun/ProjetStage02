1565c2aa5d199eb5b966c883e05806d8c8c3a61d
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-10-07 15:05:57 -0400

    EQ2-109 merged with main

--- src/main/java/projet/projetstage02/service/GestionnaireService.java
+++ src/main/java/projet/projetstage02/service/GestionnaireService.java
@@ -2,10 +2,7 @@ package projet.projetstage02.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import projet.projetstage02.DTO.CompanyDTO;
-import projet.projetstage02.DTO.GestionnaireDTO;
-import projet.projetstage02.DTO.OffreDTO;
-import projet.projetstage02.DTO.StudentDTO;
+import projet.projetstage02.DTO.*;
 import projet.projetstage02.exception.NonExistentEntityException;
 import projet.projetstage02.exception.NonExistentOfferExeption;
 import projet.projetstage02.model.Company;
