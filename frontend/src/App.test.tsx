865a156d8388635c40a847c27bbc32d71d2bd013
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-10-10 23:36:39 -0400

    EQ2-108 Added the different mock pages

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,5 +1,70 @@
 import IUser from "./models/IUser";
+import {BrowserRouter} from "react-router-dom";
+import CompanyDashboardPage from "./pages/CompanyDashboardPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
+import FormulaireSoumissionPage from "./pages/FormulaireSoumissionPage";
+import GestionnaireDashboardPage from "./pages/GestionnaireDashboardPage";
+import StudentDashboardPage from "./pages/StudentDashboardPage";
+import LoginPage from "./pages/LoginPage";
+import StudentCvValidationPage from "./pages/StudentCvValidationPage";
+import UserValidation from "./pages/UserValidationPage";
 
+const MockCompanyDashboardPage = () => {
+    return (
+        <BrowserRouter>
+            <CompanyDashboardPage  deconnexion={()=>null} user={company}/>
+        </BrowserRouter>
+    );
+}
+const MockConfirmationPage = () => {
+    return (
+        <BrowserRouter>
+            <ConfirmationPage />
+        </BrowserRouter>
+    );
+}
+const MockFormulaireSoumissionPage = () => {
+    return (
+        <BrowserRouter>
+            <FormulaireSoumissionPage user={gestionnaire}  />
+        </BrowserRouter>
+    );
+}
+const MockGestionnaireDashboardPage = () => {
+    return (
+        <BrowserRouter>
+            <GestionnaireDashboardPage user={gestionnaire} deconnexion={()=>null}/>
+        </BrowserRouter>
+    );
+}
+const MockStudentDashboardPage = () => {
+    return (
+        <BrowserRouter>
+            <StudentDashboardPage user={student} deconnexion={()=>null}/>
+        </BrowserRouter>
+    );
+}
+const MockLoginPage = () => {
+    return (
+        <BrowserRouter>
+            <LoginPage setUser={()=>null}/>
+        </BrowserRouter>
+    );
+}
+const MockStudentCvValidationPage = () => {
+    return (
+        <BrowserRouter>
+            <StudentCvValidationPage connectedUser={gestionnaire} deconnexion={()=>null}/>
+        </BrowserRouter>
+    );
+}
+const MockUserValidationPage = () => {
+    return (
+        <BrowserRouter>
+            <UserValidation connectedUser={gestionnaire} />
+        </BrowserRouter>
+    );
+}
 const gestionnaire: IUser = {
     firstName: "Yan",
     lastName: "Zhou",
@@ -11,6 +76,12 @@ const company: IUser = {
     firstName: "Bob",
     lastName: "Marley",
     userType: "company",
+    token: "324324332"
+}
+const student: IUser = {
+    firstName: "Bartholomew",
+    lastName: "Kuma",
+    userType: "student",
     token: "324324332"
 }
 
