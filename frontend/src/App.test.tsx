70081c33000d51871c6a99f913f32a385a69018c
Mathieu Ford <mathieu.ford@gmail.com>
2022-10-12 15:36:21 -0400

    EQ2-90 Added tests for OffersListPage

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,9 @@ import { act, fireEvent, render, screen, waitForElementToBeRemoved } from "@test
 import { BrowserRouter } from "react-router-dom";
 import { emptyUser } from "./App";
 import StudentCvValidationPage from "./pages/StudentCvValidationPage";
+import OffersListPage from "./pages/OffersListPage"
 
-describe("StudentCvValidationPage test", () => {
+describe("StudentCvValidationPageTests", () => {
 
     const MockPage = () => {
         return (
@@ -27,7 +28,6 @@ describe("StudentCvValidationPage test", () => {
                 });
             }
             else if (url === "http://localhost:8080/validateCv/1") {
-                console.log("validate")
                 return Promise.resolve({
                     ok: true
                 });
@@ -37,35 +37,43 @@ describe("StudentCvValidationPage test", () => {
                     ok: true
                 });
             }
-            else if (url === "http://localhost:8080/studentCv/") {
+            else if (url === "http://localhost:8080/studentCv/1") {
                 return Promise.resolve({
                     ok: true,
-                    json: () => Promise.resolve([{
-                        "prf": []
-                    }])
+                    json: () => Promise.resolve({
+                        "pdf": "[10]"
+                    })
                 });
             }
-            else throw new Error("Bad url call")
+            else throw new Error("Bad url call");
         }
         ) as jest.Mock;
+
+        const intersectionObserverMock = () => ({
+            observe: () => null,
+            unobserve: () => null,
+            disconnect: () => null
+        })
+        window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverMock);
 
         window.alert = jest.fn(() => null) as jest.Mock;
     })
 
     it("fetchUnvalidatedCvStudentsTest", async () => {
         act(() => {
-            render(<MockPage />)
+            render(<MockPage />);
         });
 
         expect(await screen.findByText(/Bart/i)).toBeInTheDocument();
         expect(await screen.findByText(/Simpson/i)).toBeInTheDocument();
         expect(await screen.findByText(/Informatique/i)).toBeInTheDocument();
+        expect(fetch).toBeCalledWith("http://localhost:8080/unvalidatedCvStudents", expect.anything());
         expect(fetch).toBeCalledTimes(1);
     });
 
     it("validateCvTest", async () => {
         act(() => {
-            render(<MockPage />)
+            render(<MockPage />);
         });
         const validateButton = await screen.findByRole("button", { name: /O/i });
 
@@ -80,7 +88,7 @@ describe("StudentCvValidationPage test", () => {
 
     it("refuseCvTest", async () => {
         act(() => {
-            render(<MockPage />)
+            render(<MockPage />);
         });
         const validateButton = await screen.findByRole("button", { name: /X/i });
 
@@ -95,19 +103,100 @@ describe("StudentCvValidationPage test", () => {
 
     it("getPDFTest", async () => {
         act(() => {
-            render(<MockPage />)
+            render(<MockPage />);
         });
+
         const cvButton = await screen.findByRole("button", { name: /CV/i });
 
         act(() => {
             fireEvent.click(cvButton);
         });
 
+        expect(await screen.findByRole("button", { name: /Fermer/i })).toBeInTheDocument();
         expect(fetch).toBeCalledWith("http://localhost:8080/studentCv/1", expect.anything());
         expect(fetch).toBeCalledTimes(2);
     });
 
     afterEach(() => {
         expect(fetch).toBeCalledWith("http://localhost:8080/unvalidatedCvStudents", expect.anything());
+    });
+});
+
+describe("OffersListPageTests", () => {
+
+    const MockPage = () => {
+        const user = emptyUser;
+        user.id = "1";
+
+        return (
+            <BrowserRouter>
+                <OffersListPage connectedUser={user} deconnexion={() => null} />
+            </BrowserRouter>
+        );
+    }
+
+    beforeEach(() => {
+        global.fetch = jest.fn((url) => {
+            if (url === "http://localhost:8080/getOffers/1") {
+                return Promise.resolve({
+                    ok: true,
+                    json: () => Promise.resolve([{
+                        "id": 2,
+                        "nomDeCompagnie": "Duff Beer",
+                        "position": "Delivery Man",
+                        "heureParSemaine": "40",
+                        "adresse": "123 Joe street"
+                    }])
+                });
+            }
+            else if (url === "http://localhost:8080/getOfferStudent/2") {
+                return Promise.resolve({
+                    ok: true,
+                    json: () => Promise.resolve({
+                        "pdf": "[10]"
+                    })
+                });
+            }
+            else throw new Error("Bad url call");
+        }) as jest.Mock;
+
+
+        const intersectionObserverMock = () => ({
+            observe: () => null,
+            unobserve: () => null,
+            disconnect: () => null
+        })
+        window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverMock);
+    });
+
+    it("getOfferPDFTest", async () => {
+        act(() => {
+            render(<MockPage />);
+        });
+
+        const pdfButton = await screen.findByRole("button", { name: /PDF/i });
+
+        act(() => {
+            fireEvent.click(pdfButton);
+        });
+
+        expect(await screen.findByRole("button", { name: /Fermer/i })).toBeInTheDocument();
+        expect(fetch).toBeCalledWith("http://localhost:8080/getOffers/1", expect.anything());
+        expect(fetch).toBeCalledWith("http://localhost:8080/getOfferStudent/2", expect.anything());
+        expect(fetch).toBeCalledTimes(2);
+    });
+
+    it("fetchOffersTest", async () => {
+        act(() => {
+            render(<MockPage />);
+        });
+
+        expect(await screen.findByText(/Duff Beer/i)).toBeInTheDocument();
+        expect(await screen.findByText(/40/i)).toBeInTheDocument();
+        expect(await screen.findByText(/Delivery Man/i)).toBeInTheDocument();
+        expect(await screen.findByText(/40/i)).toBeInTheDocument();
+        expect(await screen.findByText(/123 joe street/i)).toBeInTheDocument();
+        expect(fetch).toBeCalledWith("http://localhost:8080/getOffers/1", expect.anything());
+        expect(fetch).toBeCalledTimes(1);
     });
 });
