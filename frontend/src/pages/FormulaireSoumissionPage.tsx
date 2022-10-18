02e3c93db71dfda3831248270fdabe80380430fc
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-10-18 12:03:42 -0400

    main fixed pdf upload again

--- frontend/src/pages/FormulaireSoumissionPage.tsx
+++ frontend/src/pages/FormulaireSoumissionPage.tsx
@@ -5,7 +5,7 @@ import IUser from "../models/IUser";
 
 declare type FormControlElement = HTMLInputElement | HTMLTextAreaElement;
 
-const FormulaireSoumissionPage = ({ user }: { user: IUser }): JSX.Element => {
+const FormulaireSoumissionPage = ({user}: { user: IUser }): JSX.Element => {
     const [waiting, setWaiting] = useState(false);
     const [validated, setValidated] = useState(false);
     const [company, setCompany] = useState("")
@@ -13,7 +13,7 @@ const FormulaireSoumissionPage = ({ user }: { user: IUser }): JSX.Element => {
     const [poste, setPoste] = useState("")
     const [hoursPerWeek, setHoursPerWeek] = useState(40)
     const [address, setAddress] = useState("")
-    const [pdf, setPdf] = useState([0])
+    const [pdf, setPdf] = useState<number[]>([])
 
     const onSubmit = async (event: React.SyntheticEvent): Promise<void> => {
         const form: any = event.currentTarget;
@@ -32,7 +32,7 @@ const FormulaireSoumissionPage = ({ user }: { user: IUser }): JSX.Element => {
 
             const headers = {
                 method: "POST",
-                headers: { "Content-Type": "application/json" },
+                headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(obj)
             };
             const res = await fetch("http://localhost:8080/createOffre", headers)
@@ -56,21 +56,11 @@ const FormulaireSoumissionPage = ({ user }: { user: IUser }): JSX.Element => {
         }
     }
 
-    //source: https://stackoverflow.com/questions/8482309/converting-javascript-integer-to-byte-array-and-back
-    const intToByteArray = (array: Uint8Array) => {
+    const intToByteArray = (array: Uint8Array): number[] => {
         let bytes: number[] = []
-        for (let i = 0; i < array.length; i++) {
-
-            let long = array[i]
-            let byteArray = [0, 0, 0, 0];
-            // we want to represent the input as a 4-bytes array
-            for (let index = 0; index < byteArray.length; index++) {
-                let byte = long & 0xff;
-                byteArray[index] = byte;
-                long = (long - byte) / 256;
-            }
-            bytes.push(...byteArray)
-        }
+        array.forEach(value => {
+            bytes.push(value)
+        })
         return bytes;
     };
 
@@ -83,7 +73,7 @@ const FormulaireSoumissionPage = ({ user }: { user: IUser }): JSX.Element => {
     if (waiting) {
         return (
             <div className="d-flex justify-content-center py-5 bg-light">
-                <BeatLoader className="text-center" color="#292b2c" size={100} />
+                <BeatLoader className="text-center" color="#292b2c" size={100}/>
             </div>
         );
     }
@@ -96,13 +86,13 @@ const FormulaireSoumissionPage = ({ user }: { user: IUser }): JSX.Element => {
                     <Form.Group>
                         <Form.Label className="fw-bold h5">Nom de la compagnie</Form.Label>
                         <Form.Control data-testid="nomCompanyFormulaireSoumission" type="text" required value={company}
-                            onChange={field => setCompany(field.target.value)}></Form.Control>
+                                      onChange={field => setCompany(field.target.value)}></Form.Control>
                         <Form.Control.Feedback type="invalid">Champ requis</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="fw-bold mt-2 h5">Département</Form.Label>
                         <Form.Select data-testid="departmentFormulaireSoumission" required
-                            value={department} onChange={(e) => setDepartment(e.target.value)}>
+                                     value={department} onChange={(e) => setDepartment(e.target.value)}>
                             <option hidden value="" disabled>Choix d'un département</option>
                             <option value="Techniques de linformatique">
                                 Technique de l'informatique
@@ -116,32 +106,34 @@ const FormulaireSoumissionPage = ({ user }: { user: IUser }): JSX.Element => {
                     <Form.Group>
                         <Form.Label className="fw-bold h5">Poste</Form.Label>
                         <Form.Control data-testid="posteFormulaireSoumission" type="text" required value={poste}
-                            onChange={field => setPoste(field.target.value)}></Form.Control>
+                                      onChange={field => setPoste(field.target.value)}></Form.Control>
                         <Form.Control.Feedback type="invalid">Champ requis</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="fw-bold h5">Heures par semaine</Form.Label>
-                        <Form.Control data-testid="hoursFormulaireSoumission" id={"number"} type="number" min="1" max="40" required value={hoursPerWeek}
-                            onChange={field => setHoursPerWeekFromField(field)}></Form.Control>
+                        <Form.Control data-testid="hoursFormulaireSoumission" id={"number"} type="number" min="1"
+                                      max="40" required value={hoursPerWeek}
+                                      onChange={field => setHoursPerWeekFromField(field)}></Form.Control>
                         <Form.Control.Feedback type="invalid">Nombre d'heures entre 0 et 40</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="fw-bold h5">Adresse</Form.Label>
                         <Form.Control data-testid="addressFormulaireSoumission" type="text" required value={address}
-                            onChange={field => setAddress(field.target.value)}></Form.Control>
+                                      onChange={field => setAddress(field.target.value)}></Form.Control>
                         <Form.Control.Feedback type="invalid">Champ requis</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="fw-bold h5">Document PDF</Form.Label>
                         <input data-testid="pdfFormulaireSoumission" id={"file"} className="form-control" accept=".pdf"
-                            required type="file" onChange={(e) => {
-                                uploadFile(e.target.files![0]);
-                            }} />
+                               required type="file" onChange={(e) => {
+                            uploadFile(e.target.files![0]);
+                        }}/>
                         <Form.Control.Feedback type="invalid">Champ requis</Form.Control.Feedback>
                     </Form.Group>
                     <Row className="mt-3">
                         <Col className="text-center">
-                            <Button data-testid="envoyerFormulaireSoumission" type="submit" className="btn btn-success mx-auto w-75">Envoyer</Button>
+                            <Button data-testid="envoyerFormulaireSoumission" type="submit"
+                                    className="btn btn-success mx-auto w-75">Envoyer</Button>
                         </Col>
                     </Row>
                 </Form>
