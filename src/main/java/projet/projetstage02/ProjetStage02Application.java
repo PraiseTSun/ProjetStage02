96689c6d5940a3ac8d7e660dbdf53867e0e4378d
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-10-19 14:46:19 -0400

    EQ2-93 added the new endpoint for voir liste etudiant postulés

--- src/main/java/projet/projetstage02/ProjetStage02Application.java
+++ src/main/java/projet/projetstage02/ProjetStage02Application.java
@@ -15,125 +15,125 @@ import projet.projetstage02.service.StudentService;
 @AllArgsConstructor
 public class ProjetStage02Application implements CommandLineRunner {
 
-        private StudentService studentService;
+    private StudentService studentService;
 
-        private CompanyService companyService;
+    private CompanyService companyService;
 
-        private GestionnaireService gestionnaireService;
+    private GestionnaireService gestionnaireService;
 
-        private final byte[] TESTPDF = new byte[]{
-                37, 80, 68, 70, 45, 49, 46, 55, 10, 10, 49, 32, 48, 32, 111, 98, 106, 32, 32, 37, 32, 101, 110, 116,
-                114, 121, 32, 112, 111, 105, 110, 116, 10, 60, 60, 10, 32, 32, 47, 84, 121, 112, 101, 32, 47, 67,
-                97, 116, 97, 108, 111, 103, 10, 32, 32, 47, 80, 97, 103, 101, 115, 32, 50, 32, 48, 32, 82, 10, 62,
-                62, 10, 101, 110, 100, 111, 98, 106, 10, 10, 50, 32, 48, 32, 111, 98, 106, 10, 60, 60, 10, 32, 32,
-                47, 84, 121, 112, 101, 32, 47, 80, 97, 103, 101, 115, 10, 32, 32, 47, 77, 101, 100, 105, 97, 66,
-                111, 120, 32, 91, 32, 48, 32, 48, 32, 50, 48, 48, 32, 50, 48, 48, 32, 93, 10, 32, 32, 47, 67, 111,
-                117, 110, 116, 32, 49, 10, 32, 32, 47, 75, 105, 100, 115, 32, 91, 32, 51, 32, 48, 32, 82, 32, 93,
-                10, 62, 62, 10, 101, 110, 100, 111, 98, 106, 10, 10, 51, 32, 48, 32, 111, 98, 106, 10, 60, 60, 10,
-                32, 32, 47, 84, 121, 112, 101, 32, 47, 80, 97, 103, 101, 10, 32, 32, 47, 80, 97, 114, 101, 110, 116,
-                32, 50, 32, 48, 32, 82, 10, 32, 32, 47, 82, 101, 115, 111, 117, 114, 99, 101, 115, 32, 60, 60, 10,
-                32, 32, 32, 32, 47, 70, 111, 110, 116, 32, 60, 60, 10, 32, 32, 32, 32, 32, 32, 47, 70, 49, 32, 52,
-                32, 48, 32, 82, 32, 10, 32, 32, 32, 32, 62, 62, 10, 32, 32, 62, 62, 10, 32, 32, 47, 67, 111, 110,
-                116, 101, 110, 116, 115, 32, 53, 32, 48, 32, 82, 10, 62, 62, 10, 101, 110, 100, 111, 98, 106, 10,
-                10, 52, 32, 48, 32, 111, 98, 106, 10, 60, 60, 10, 32, 32, 47, 84, 121, 112, 101, 32, 47, 70, 111,
-                110, 116, 10, 32, 32, 47, 83, 117, 98, 116, 121, 112, 101, 32, 47, 84, 121, 112, 101, 49, 10, 32,
-                32, 47, 66, 97, 115, 101, 70, 111, 110, 116, 32, 47, 84, 105, 109, 101, 115, 45, 82, 111, 109, 97,
-                110, 10, 62, 62, 10, 101, 110, 100, 111, 98, 106, 10, 10, 53, 32, 48, 32, 111, 98, 106, 32, 32, 37,
-                32, 112, 97, 103, 101, 32, 99, 111, 110, 116, 101, 110, 116, 10, 60, 60, 10, 32, 32, 47, 76, 101,
-                110, 103, 116, 104, 32, 52, 52, 10, 62, 62, 10, 115, 116, 114, 101, 97, 109, 10, 66, 84, 10, 55, 48,
-                32, 53, 48, 32, 84, 68, 10, 47, 70, 49, 32, 49, 50, 32, 84, 102, 10, 40, 72, 101, 108, 108, 111, 44,
-                32, 119, 111, 114, 108, 100, 33, 41, 32, 84, 106, 10, 69, 84, 10, 101, 110, 100, 115, 116, 114, 101,
-                97, 109, 10, 101, 110, 100, 111, 98, 106, 10, 10, 120, 114, 101, 102, 10, 48, 32, 54, 10, 48, 48,
-                48, 48, 48, 48, 48, 48, 48, 48, 32, 54, 53, 53, 51, 53, 32, 102, 32, 10, 48, 48, 48, 48, 48, 48, 48,
-                48, 49, 48, 32, 48, 48, 48, 48, 48, 32, 110, 32, 10, 48, 48, 48, 48, 48, 48, 48, 48, 55, 57, 32, 48,
-                48, 48, 48, 48, 32, 110, 32, 10, 48, 48, 48, 48, 48, 48, 48, 49, 55, 51, 32, 48, 48, 48, 48, 48, 32,
-                110, 32, 10, 48, 48, 48, 48, 48, 48, 48, 51, 48, 49, 32, 48, 48, 48, 48, 48, 32, 110, 32, 10, 48,
-                48, 48, 48, 48, 48, 48, 51, 56, 48, 32, 48, 48, 48, 48, 48, 32, 110, 32, 10, 116, 114, 97, 105, 108,
-                101, 114, 10, 60, 60, 10, 32, 32, 47, 83, 105, 122, 101, 32, 54, 10, 32, 32, 47, 82, 111, 111, 116,
-                32, 49, 32, 48, 32, 82, 10, 62, 62, 10, 115, 116, 97, 114, 116, 120, 114, 101, 102, 10, 52, 57, 50,
-                10, 37, 37, 69, 79, 70
-        };
+    private final byte[] TESTPDF = new byte[]{
+            37, 80, 68, 70, 45, 49, 46, 55, 10, 10, 49, 32, 48, 32, 111, 98, 106, 32, 32, 37, 32, 101, 110, 116,
+            114, 121, 32, 112, 111, 105, 110, 116, 10, 60, 60, 10, 32, 32, 47, 84, 121, 112, 101, 32, 47, 67,
+            97, 116, 97, 108, 111, 103, 10, 32, 32, 47, 80, 97, 103, 101, 115, 32, 50, 32, 48, 32, 82, 10, 62,
+            62, 10, 101, 110, 100, 111, 98, 106, 10, 10, 50, 32, 48, 32, 111, 98, 106, 10, 60, 60, 10, 32, 32,
+            47, 84, 121, 112, 101, 32, 47, 80, 97, 103, 101, 115, 10, 32, 32, 47, 77, 101, 100, 105, 97, 66,
+            111, 120, 32, 91, 32, 48, 32, 48, 32, 50, 48, 48, 32, 50, 48, 48, 32, 93, 10, 32, 32, 47, 67, 111,
+            117, 110, 116, 32, 49, 10, 32, 32, 47, 75, 105, 100, 115, 32, 91, 32, 51, 32, 48, 32, 82, 32, 93,
+            10, 62, 62, 10, 101, 110, 100, 111, 98, 106, 10, 10, 51, 32, 48, 32, 111, 98, 106, 10, 60, 60, 10,
+            32, 32, 47, 84, 121, 112, 101, 32, 47, 80, 97, 103, 101, 10, 32, 32, 47, 80, 97, 114, 101, 110, 116,
+            32, 50, 32, 48, 32, 82, 10, 32, 32, 47, 82, 101, 115, 111, 117, 114, 99, 101, 115, 32, 60, 60, 10,
+            32, 32, 32, 32, 47, 70, 111, 110, 116, 32, 60, 60, 10, 32, 32, 32, 32, 32, 32, 47, 70, 49, 32, 52,
+            32, 48, 32, 82, 32, 10, 32, 32, 32, 32, 62, 62, 10, 32, 32, 62, 62, 10, 32, 32, 47, 67, 111, 110,
+            116, 101, 110, 116, 115, 32, 53, 32, 48, 32, 82, 10, 62, 62, 10, 101, 110, 100, 111, 98, 106, 10,
+            10, 52, 32, 48, 32, 111, 98, 106, 10, 60, 60, 10, 32, 32, 47, 84, 121, 112, 101, 32, 47, 70, 111,
+            110, 116, 10, 32, 32, 47, 83, 117, 98, 116, 121, 112, 101, 32, 47, 84, 121, 112, 101, 49, 10, 32,
+            32, 47, 66, 97, 115, 101, 70, 111, 110, 116, 32, 47, 84, 105, 109, 101, 115, 45, 82, 111, 109, 97,
+            110, 10, 62, 62, 10, 101, 110, 100, 111, 98, 106, 10, 10, 53, 32, 48, 32, 111, 98, 106, 32, 32, 37,
+            32, 112, 97, 103, 101, 32, 99, 111, 110, 116, 101, 110, 116, 10, 60, 60, 10, 32, 32, 47, 76, 101,
+            110, 103, 116, 104, 32, 52, 52, 10, 62, 62, 10, 115, 116, 114, 101, 97, 109, 10, 66, 84, 10, 55, 48,
+            32, 53, 48, 32, 84, 68, 10, 47, 70, 49, 32, 49, 50, 32, 84, 102, 10, 40, 72, 101, 108, 108, 111, 44,
+            32, 119, 111, 114, 108, 100, 33, 41, 32, 84, 106, 10, 69, 84, 10, 101, 110, 100, 115, 116, 114, 101,
+            97, 109, 10, 101, 110, 100, 111, 98, 106, 10, 10, 120, 114, 101, 102, 10, 48, 32, 54, 10, 48, 48,
+            48, 48, 48, 48, 48, 48, 48, 48, 32, 54, 53, 53, 51, 53, 32, 102, 32, 10, 48, 48, 48, 48, 48, 48, 48,
+            48, 49, 48, 32, 48, 48, 48, 48, 48, 32, 110, 32, 10, 48, 48, 48, 48, 48, 48, 48, 48, 55, 57, 32, 48,
+            48, 48, 48, 48, 32, 110, 32, 10, 48, 48, 48, 48, 48, 48, 48, 49, 55, 51, 32, 48, 48, 48, 48, 48, 32,
+            110, 32, 10, 48, 48, 48, 48, 48, 48, 48, 51, 48, 49, 32, 48, 48, 48, 48, 48, 32, 110, 32, 10, 48,
+            48, 48, 48, 48, 48, 48, 51, 56, 48, 32, 48, 48, 48, 48, 48, 32, 110, 32, 10, 116, 114, 97, 105, 108,
+            101, 114, 10, 60, 60, 10, 32, 32, 47, 83, 105, 122, 101, 32, 54, 10, 32, 32, 47, 82, 111, 111, 116,
+            32, 49, 32, 48, 32, 82, 10, 62, 62, 10, 115, 116, 97, 114, 116, 120, 114, 101, 102, 10, 52, 57, 50,
+            10, 37, 37, 69, 79, 70
+    };
 
-        public static void main(String[] args) {
-                SpringApplication.run(ProjetStage02Application.class, args);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetStage02Application.class, args);
+    }
 
-        @Override
-        public void run(String... args) throws Exception {
-                studentService.saveStudent("Samir", "Badi", "Samir@gmail.com", "cooldude",
-                                AbstractUser.Department.Informatique);
-                StudentDTO student = studentService.getStudentById(1L);
-                student.setEmailConfirmed(true);
-                studentService.saveStudent(student);
+    @Override
+    public void run(String... args) throws Exception {
+        studentService.saveStudent("Samir", "Badi", "Samir@gmail.com", "cooldude",
+                AbstractUser.Department.Informatique);
+        StudentDTO student = studentService.getStudentById(1L);
+        student.setEmailConfirmed(true);
+        studentService.saveStudent(student);
 
-                companyService.saveCompany("Bob", "Marley", "Bell", "Bob@bell.com", "bestcompany",
-                                AbstractUser.Department.Informatique);
-                CompanyDTO company = companyService.getCompanyById(2L);
-                company.setEmailConfirmed(true);
-                companyService.saveCompany(company);
+        companyService.saveCompany("Bob", "Marley", "Bell", "Bob@bell.com", "bestcompany",
+                AbstractUser.Department.Informatique);
+        CompanyDTO company = companyService.getCompanyById(2L);
+        company.setEmailConfirmed(true);
+        companyService.saveCompany(company);
 
-                gestionnaireService.saveGestionnaire("Dave", "Douch", "dave@gmail.ca", "cooldude");
-                GestionnaireDTO gestionnaire = gestionnaireService.getGestionnaireById(3L);
-                gestionnaire.setEmailConfirmed(true);
-                gestionnaireService.saveGestionnaire(gestionnaire);
+        gestionnaireService.saveGestionnaire("Dave", "Douch", "dave@gmail.ca", "cooldude");
+        GestionnaireDTO gestionnaire = gestionnaireService.getGestionnaireById(3L);
+        gestionnaire.setEmailConfirmed(true);
+        gestionnaireService.saveGestionnaire(gestionnaire);
 
-                studentService.saveStudent("Peter", "Griffin", "peter.griffin@quahog.com", "lois",
-                                AbstractUser.Department.Informatique);
-                StudentDTO student2 = studentService.getStudentByEmailPassword("peter.griffin@quahog.com", "lois");
-                student2.setEmailConfirmed(true);
-                studentService.saveStudent(student2);
+        studentService.saveStudent("Peter", "Griffin", "peter.griffin@quahog.com", "loislois",
+                AbstractUser.Department.Informatique);
+        StudentDTO student2 = studentService.getStudentByEmailPassword("peter.griffin@quahog.com", "loislois");
+        student2.setEmailConfirmed(true);
+        studentService.saveStudent(student2);
 
-                long offreId = companyService.createOffre(OffreDTO.builder()
-                                .adresse("123 Joe Road")
-                                .department(Department.Informatique.departement)
-                                .heureParSemaine(40)
-                                .salaire(40)
-                                .nomDeCompagnie("Duff Beer")
-                                .position("Delivery Man")
-                                .pdf(TESTPDF)
-                                .build());
-                gestionnaireService.validateOfferById(offreId);
+        long offreId = companyService.createOffre(OffreDTO.builder()
+                .adresse("123 Joe Road")
+                .department(Department.Informatique.departement)
+                .heureParSemaine(40)
+                .salaire(40)
+                .nomDeCompagnie("Duff Beer")
+                .position("Delivery Man")
+                .pdf(TESTPDF)
+                .build());
+        gestionnaireService.validateOfferById(offreId);
 
-                studentService.uploadCurriculumVitae(PdfDTO.builder()
-                                .studentId(1L)
-                                .pdf(TESTPDF)
-                                .build());
+        studentService.uploadCurriculumVitae(PdfDTO.builder()
+                .studentId(1L)
+                .pdf(TESTPDF)
+                .build());
 
-                studentService.uploadCurriculumVitae(PdfDTO.builder()
-                                .studentId(student2.getId())
-                                .pdf(TESTPDF)
-                                .build());
+        studentService.uploadCurriculumVitae(PdfDTO.builder()
+                .studentId(student2.getId())
+                .pdf(TESTPDF)
+                .build());
 
-                companyService.createOffre(new OffreDTO(0L,
-                                "Bell",
-                                "Techniques de linformatique",
-                                "Support TI",
-                                35,
-                                35,
-                                "My Home",
-                                TESTPDF,
-                                "notoken",
-                                false));
-                companyService.createOffre(new OffreDTO(0L,
-                                "Bell BB",
-                                "Techniques de linformatique",
-                                "Support TI",
-                                36,
-                                15,
-                                "33 My Home",
-                                TESTPDF,
-                                "notoken",
-                                false));
-                gestionnaireService.validateStudent(student.getId());
-                gestionnaireService.validateStudent(student2.getId());
-                System.out.println(studentService.getStudentById(1L));
-                System.out.println(companyService.getCompanyById(2L));
-                System.out.println(gestionnaireService.getGestionnaireById(3L));
-                System.out.println(gestionnaireService.getGestionnaireById(3L));
-                System.out.println(studentService.getStudentByEmailPassword("Samir@gmail.com", "cooldude"));
-                System.out.println(companyService.getCompanyByEmailPassword("Bob@bell.com", "bestcompany"));
-                System.out.println(gestionnaireService.getGestionnaireByEmailPassword("dave@gmail.ca", "cooldude"));
-                System.out.println(gestionnaireService.getUnvalidatedOffers());
-                System.out.println(gestionnaireService.getUnvalidatedCVStudents());
-        }
+        companyService.createOffre(new OffreDTO(0L,
+                "Bell",
+                "Techniques de linformatique",
+                "Support TI",
+                35,
+                35,
+                "My Home",
+                TESTPDF,
+                "notoken",
+                false));
+        companyService.createOffre(new OffreDTO(0L,
+                "Bell BB",
+                "Techniques de linformatique",
+                "Support TI",
+                36,
+                15,
+                "33 My Home",
+                TESTPDF,
+                "notoken",
+                false));
+        gestionnaireService.validateStudent(student.getId());
+        gestionnaireService.validateStudent(student2.getId());
+        System.out.println(studentService.getStudentById(1L));
+        System.out.println(companyService.getCompanyById(2L));
+        System.out.println(gestionnaireService.getGestionnaireById(3L));
+        System.out.println(gestionnaireService.getGestionnaireById(3L));
+        System.out.println(studentService.getStudentByEmailPassword("Samir@gmail.com", "cooldude"));
+        System.out.println(companyService.getCompanyByEmailPassword("Bob@bell.com", "bestcompany"));
+        System.out.println(gestionnaireService.getGestionnaireByEmailPassword("dave@gmail.ca", "cooldude"));
+        System.out.println(gestionnaireService.getUnvalidatedOffers());
+        System.out.println(gestionnaireService.getUnvalidatedCVStudents());
+    }
 }
