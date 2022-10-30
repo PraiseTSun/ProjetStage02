a9ec58404a42d0be9ababfa94683815d894c2c4b
Mathieu Ford <mathieu.ford@gmail.com>
2022-10-30 17:53:44 -0400

    EQ2-126 Can now write to canvas for student signature

--- frontend/src/pages/studentPages/StudentContractsPage.tsx
+++ frontend/src/pages/studentPages/StudentContractsPage.tsx
@@ -5,11 +5,13 @@ import PageHeader from "../../components/universalComponents/PageHeader";
 import IContract from "../../models/IContract";
 import {putStudentContracts} from "../../services/studentServices/StudentFetchService";
 import {generateAlert} from "../../services/universalServices/UniversalUtilService";
+import SignaturePad from "react-signature-canvas"
 
 const StudentContractsPage = ({connectedUser}: { connectedUser: IUser }): JSX.Element => {
     const nextYear = new Date().getFullYear() + 1
     const [signing, setSigning] = useState<boolean>(false);
     const [contrats, setContrats] = useState<IContract[]>([]);
+    let sigPad: SignaturePad | null
 
     const fetchContracts = useCallback(async () => {
         try {
@@ -30,15 +32,37 @@ const StudentContractsPage = ({connectedUser}: { connectedUser: IUser }): JSX.El
         fetchContracts();
     }, [fetchContracts, connectedUser])
 
+
     if (signing) {
         return (
-            <Container className="vh-100 bg-light">
+            <Container className="vh-100">
                 <Row className="bg-dark p-2">
                     <Col sm={1}><Button variant="danger" onClick={() => {
                         setSigning(false)
                     }}>Fermer</Button></Col>
                     <Col sm={10}></Col>
-                    <Col sm={1}><Button variant="success">Signer</Button></Col>
+                    <Col sm={1}><Button variant="success" onClick={() => {
+                        if (sigPad!.isEmpty()) {
+                            alert("Vous devez signer!")
+                        } else {
+                            sigPad?.getCanvas().toBlob((blob) => {
+                                blob?.arrayBuffer().then((data) => {
+                                    console.log(new Uint8Array(data));
+                                })
+                            })
+                        }
+                    }}>Signer</Button></Col>
+                </Row>
+                <Row>
+                    <Col sm={4} className="mx-auto mt-3">
+                        <SignaturePad canvasProps={{width: 500, height: 200, className: 'border border-5 bg-light'}}
+                                      ref={(ref) => {
+                                          sigPad = ref
+                                      }}/>
+                        <Button onClick={() => {
+                            sigPad!.clear()
+                        }}>Recommencer</Button>
+                    </Col>
                 </Row>
             </Container>
         );
