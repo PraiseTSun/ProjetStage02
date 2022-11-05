b93cd90a3a5e4a1fa5a145b53466f8a98f6289ae
Yan Zhou <zhouyan87910@gmail.com>
2022-11-04 20:15:18 -0400

    EQ2-123 corriger des champs du formulaire

--- src/main/java/projet/projetstage02/controller/RootController.java
+++ src/main/java/projet/projetstage02/controller/RootController.java
@@ -842,7 +842,6 @@ public class RootController {
     @PostMapping("/evaluateStage/{token}")
     public ResponseEntity<?> evaluateStage
     (@PathVariable String token, @RequestBody EvaluationInDTO evaluationInDTO) {
-        System.out.println(evaluationInDTO);
         try {
             logger.log(Level.INFO, "put /evaluateStage/id entered with id : " + token);
             authService.getToken(token, GESTIONNAIRE);
