7899503c723203973f880ff92ef339e5cb4ea5e0
Mathieu Ford <mathieu.ford@gmail.com>
2022-11-11 23:46:07 -0500

    EQ2-149 Now checks if student was evaluated before

--- frontend/src/pages/companyPages/CompanyContractsPage.tsx
+++ frontend/src/pages/companyPages/CompanyContractsPage.tsx
@@ -1,21 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {Button, Col, Container, Row, Table} from "react-bootstrap";
 import IUser from "../../models/IUser";
 import {generateAlert} from "../../services/universalServices/UniversalUtilService";
-import {putcompanyContracts, putCompanySignatureContract} from "../../services/companyServices/CompanyFetchService";
+import {
+    putcompanyContracts,
+    putCompanySignatureContract,
+    putGetEvaluatedStudentsContrats
+} from "../../services/companyServices/CompanyFetchService";
 import SignaturePad from "react-signature-canvas";
 import PageHeader from "../../components/universalComponents/PageHeader";
 import StudentEvaluationForm from "../../components/companyComponents/StudentEvaluationForm";
+import {universalFetch} from "../../services/universalServices/UniversalFetchService";
 
 const CompanyContractsPage = ({connectedUser}: { connectedUser: IUser }): JSX.Element => {
     const [contratsNonSigner, setContratsNonSigner] = useState<any[]>([])
     const [isSigner, setIsSigner] = useState(false)
     const [contratId, setContratId] = useState(0)
+    const [evaluatedStudentContracts, setEvaluatedStudentContracts] = useState<string[]>([])
     const [currentlySelectedContract, setCurrentlySelectedContract] = useState<string>("");
     const [showEvaluationForm, setShowEvaluationForm] = useState(false)
     const nextYear: number = new Date().getFullYear() + 1
     let sigPad: SignaturePad | null
 
+    const fetchEvaluatedStudentContracts = useCallback(async () => {
+        universalFetch(async () =>
+                await putGetEvaluatedStudentsContrats(connectedUser.token, connectedUser.id),
+            async (response: Response) => {
+                const data = await response.json();
+                setEvaluatedStudentContracts(data)
+            });
+    }, [connectedUser]);
+    
     useEffect(() => {
         const fetchCompanyContracts = async () => {
             try {
@@ -31,7 +46,9 @@ const CompanyContractsPage = ({connectedUser}: { connectedUser: IUser }): JSX.El
             }
         }
         fetchCompanyContracts()
-    }, [])
+        fetchEvaluatedStudentContracts()
+    }, [fetchEvaluatedStudentContracts, connectedUser])
+
 
     async function getEntente(contratId: number): Promise<void> {
         setIsSigner(true)
@@ -121,6 +138,8 @@ const CompanyContractsPage = ({connectedUser}: { connectedUser: IUser }): JSX.El
                                         <td>{contrat.description}</td>
                                         <td>
                                             {
+                                                evaluatedStudentContracts.includes(contrat.contractId) && "Déjà évalué"
+                                                || !evaluatedStudentContracts.includes(contrat.contractId) &&
                                                 contrat.companySignature.length > 0
                                                     ? <Button variant="warning" onClick={() => {
                                                         setCurrentlySelectedContract(contrat.contractId)
