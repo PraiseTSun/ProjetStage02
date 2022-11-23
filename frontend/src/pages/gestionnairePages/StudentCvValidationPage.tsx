3d2cf56eb55eb1ba9131ca98a232566e0e18ad15
Mathieu Ford <mathieu.ford@gmail.com>
2022-11-23 17:11:23 -0500

    EQ2-165 Equalised StudentCvValidationPage

--- frontend/src/pages/gestionnairePages/StudentCvValidationPage.tsx
+++ frontend/src/pages/gestionnairePages/StudentCvValidationPage.tsx
@@ -1,8 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {Button, Col, Container, Form, Row, Table} from "react-bootstrap";
-import {Link} from "react-router-dom";
 import IUser from "../../models/IUser";
-import {Viewer} from '@react-pdf-viewer/core';
 import {
     putRefuseCv,
     putStudentUnvalidatedCv,
@@ -10,6 +8,8 @@ import {
     putValidateCv
 } from "../../services/gestionnaireServices/GestionnaireFetchService";
 import {generateAlert} from "../../services/universalServices/UniversalUtilService";
+import PageHeader from "../../components/universalComponents/PageHeader";
+import PdfComponent from "../../components/universalComponents/PdfComponent";
 
 const StudentCvValidationPage = ({connectedUser}:
                                      { connectedUser: IUser }): JSX.Element => {
@@ -20,6 +20,7 @@ const StudentCvValidationPage = ({connectedUser}:
     const [refuserMessge, setRefuserMessge] = useState<string>("")
     const [validated, setValidated] = useState<boolean>(false);
     const [studentId, setStudentId] = useState<string>("")
+
     useEffect(() => {
         const fetchUnvalidatedCvStudents = async () => {
             try {
@@ -42,20 +43,23 @@ const StudentCvValidationPage = ({connectedUser}:
         const form: any = event.currentTarget;
         event.preventDefault();
         if (form.checkValidity()) {
-
-            const response = await putRefuseCv(studentId.toString(), connectedUser.token, refuserMessge,)
-
-            if (response.ok) {
-                setIsRefuse(false)
-                alert("Message envoyé")
-            } else {
-                generateAlert()
-            }
-
-            window.location.href = "/cvValidation"
+            sendRefusalMessage();
         }
         setValidated(true);
     }
+
+    const sendRefusalMessage = useCallback(async () => {
+        const response = await putRefuseCv(studentId.toString(), connectedUser.token, refuserMessge,)
+
+        if (response.ok) {
+            setIsRefuse(false)
+            setStudents(students.filter((student) => student.id.toString() !== studentId));
+            setValidated(false)
+            setRefuserMessge("")
+        } else {
+            generateAlert()
+        }
+    }, [studentId, refuserMessge]);
 
     async function validateCV(studentId: number, valid: boolean): Promise<void> {
         if (!valid) {
@@ -67,7 +71,7 @@ const StudentCvValidationPage = ({connectedUser}:
             const response = await putValidateCv(studentId.toString(), connectedUser.token)
 
             if (response.ok) {
-                setStudents(students.filter(student => student.id !== studentId));
+                setStudents([...students.filter((student) => student.id !== studentId)]);
             } else {
                 generateAlert()
             }
@@ -94,87 +98,87 @@ const StudentCvValidationPage = ({connectedUser}:
 
     if (showPDF) {
         return (
-            <Container className="min-vh-100 bg-white p-0">
-                <div className="bg-dark p-2">
-                    <Button className="Btn btn-primary" onClick={() => setShowPDF(false)}>
-                        Fermer
-                    </Button>
-                </div>
-                <div>
-                    <Viewer fileUrl={pdf}/>
-                </div>
-            </Container>
+            <PdfComponent pdf={pdf} setShowPdf={setShowPDF}/>
         );
     }
-    if (isRefuse) {
-        return (
-            <Container className="min-vh-100">
-                <Form className="card-body p-3" onSubmit={onSubmit} validated={validated}
-                      noValidate>
-                    <Form.Group>
-                        <Form.Label className="fw-bold h5">Refuser Message</Form.Label>
-                        <Form.Control type="text" required
-                                      value={refuserMessge}
-                                      onChange={field => setRefuserMessge(field.target.value)}></Form.Control>
-                        <Form.Control.Feedback type="invalid">Champ
-                            requis</Form.Control.Feedback>
-                    </Form.Group>
-                    <Row className="mt-3">
-                        <Col className="text-center">
-                            <Button type="submit"
-                                    className="btn btn-success mx-auto w-75">Envoyer</Button>
-                        </Col>
-                    </Row>
-                </Form>
-            </Container>
-        )
-    }
+
     return (
         <Container className="min-vh-100">
+            {isRefuse &&
+                <div>
+                    <Container
+                        className="position-absolute min-vh-100 p-0 m-0 min-vw-100 bg-dark
+                            p-0 top-0 start-0 end-0 opacity-50">
+                    </Container>
+                    <Container
+                        className="position-absolute min-vh-100 min-vw-100 p-0 top-0 start-0 end-0">
+                        <Row className="min-vh-100 m-0">
+                            <Col sm={4} className="rounded m-auto bg-white">
+                                <Form className="p-3 text-center" onSubmit={onSubmit} validated={validated}
+                                      noValidate>
+                                    <Form.Group>
+                                        <Form.Label className="fw-bold h3">Message de refus</Form.Label>
+                                        <Form.Control type="text" required
+                                                      value={refuserMessge}
+                                                      onChange={field => setRefuserMessge(field.target.value)}>
+
+                                        </Form.Control>
+                                        <Form.Control.Feedback type="invalid">Champ
+                                            requis</Form.Control.Feedback>
+                                    </Form.Group>
+                                    <Row className="mt-3">
+                                        <Col className="text-center">
+                                            <Button type="submit"
+                                                    className="btn btn-success mx-auto w-75">Envoyer</Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            }
+            <PageHeader title={"Liste de CV non validé"}/>
             <Row>
-                <Col sm={2}>
-                    <Link to="/" className="btn btn-primary my-3">Home</Link>
-                </Col>
-                <Col sm={8} className="text-center pt-2">
-                    <h1 className="fw-bold text-white display-3 pb-2">Liste des CVs non validés</h1>
-                </Col>
-                <Col sm={2}></Col>
-            </Row>
-            <Row>
-                <Col>
+                <Col className="bg-light p-0" style={{height: 400}}>
                     <Table className="text-center" hover>
-                        <thead className="bg-primary text-white">
+                        <thead className="bg-primary">
                         <tr>
                             <th>Prénom</th>
                             <th>Nom</th>
-                            <th>Departement</th>
+                            <th>Département</th>
                             <th>CV</th>
                             <th>CV Valide</th>
                         </tr>
-
                         </thead>
                         <tbody className="bg-light">
-                        {students.map((student, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{student.firstName}</td>
-                                    <td>{student.lastName}</td>
-                                    <td>{student.department}</td>
-                                    <td><Button className="btn btn-warning"
-                                                onClick={async () => await getPDF(student.id)}>CV</Button></td>
-                                    <td>
-                                        <Button className="btn btn-success mx-2" onClick={
-                                            () => validateCV(student.id, true)}>
-                                            O
-                                        </Button>
-                                        <Button className="btn btn-danger" onClick={
-                                            () => validateCV(student.id, false)}>
-                                            X
-                                        </Button>
-                                    </td>
-                                </tr>
-                            );
-                        })}
+                        {students.length === 0
+                            ? <tr>
+                                <td colSpan={5}>
+                                    <p className="h1">Aucune évaluation</p>
+                                </td>
+                            </tr>
+                            : students.map((student, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{student.firstName}</td>
+                                        <td>{student.lastName}</td>
+                                        <td>{student.department}</td>
+                                        <td><Button className="btn btn-warning"
+                                                    onClick={async () => await getPDF(student.id)}>CV</Button></td>
+                                        <td>
+                                            <Button className="btn btn-success mx-2" onClick={
+                                                () => validateCV(student.id, true)}>
+                                                O
+                                            </Button>
+                                            <Button className="btn btn-danger" onClick={
+                                                () => validateCV(student.id, false)}>
+                                                X
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
                         </tbody>
                     </Table>
                 </Col>
