5f650d606121e67a6cb6a614a7f81cb5d750a4cd
Mathieu Ford <mathieu.ford@gmail.com>
2022-11-23 16:27:20 -0500

    EQ2-165 Equalised SignerEntenteDeStageParGestionnaire

--- frontend/src/pages/gestionnairePages/SignerEntenteDeStageParGestionnaire.tsx
+++ frontend/src/pages/gestionnairePages/SignerEntenteDeStageParGestionnaire.tsx
@@ -2,16 +2,15 @@ import React, {useEffect, useState} from "react";
 import {Button, Col, Container, Row, Table} from "react-bootstrap";
 import IUser from "../../models/IUser";
 import {generateAlert} from "../../services/universalServices/UniversalUtilService";
-import SignaturePad from "react-signature-canvas";
 import PageHeader from "../../components/universalComponents/PageHeader";
 import {putContracts, putSignatureContract} from "../../services/gestionnaireServices/GestionnaireFetchService";
 import StageContract from "../../models/StageContract";
+import SignaturePopup from "../../components/universalComponents/SignaturePopup";
 
 const SignerEntenteDeStageParGestionnaire = ({connectedUser}: { connectedUser: IUser }): JSX.Element => {
     const [contratsNonSigner, setContratsNonSigner] = useState<StageContract[]>([])
     const [isSigner, setIsSigner] = useState(false)
     const [contratId, setContratId] = useState(0)
-    let sigPad: SignaturePad | null
 
     useEffect(() => {
         const fetchContracts = async () => {
@@ -48,7 +47,6 @@ const SignerEntenteDeStageParGestionnaire = ({connectedUser}: { connectedUser: I
                     }
                 })
                 setContratsNonSigner([...contratsNonSigner])
-                alert("Félicitations vous avez signé le contrat")
             } else {
                 generateAlert()
             }
@@ -57,45 +55,15 @@ const SignerEntenteDeStageParGestionnaire = ({connectedUser}: { connectedUser: I
         }
     }
 
-    if (isSigner) {
-        return (
-            <Container className="vh-100">
-                <Row className="bg-dark p-2">
-                    <Col sm={1}><Button variant="danger" onClick={() => {
-                        setIsSigner(false)
-                    }}>Fermer</Button></Col>
-                    <Col sm={10}></Col>
-                    <Col sm={1}><Button variant="success" onClick={() => {
-                        if (sigPad!.isEmpty()) {
-                            alert("Vous devez signer!")
-                        } else {
-                            signer(sigPad!.toDataURL())
-                        }
-                    }}>Signer</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={4} className="mx-auto mt-3">
-                        <SignaturePad canvasProps={{width: 500, height: 200, className: 'border border-5 bg-light'}}
-                                      ref={(ref) => {
-                                          sigPad = ref
-                                      }}/>
-                        <Button onClick={() => {
-                            sigPad!.clear()
-                        }}>Recommencer</Button>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
-
     return (
-        <Container className="d-flex justify-content-center vh-100">
+        <Container className="min-vh-100">
+            <PageHeader title={"Signer Entente De Stage"}/>
+            {isSigner && <SignaturePopup setSigning={setIsSigner} onSignature={signer}/>}
             <Col>
-                <PageHeader title={"Signer Entente De Stage"}/>
                 <Row className="mt-5">
-                    <Col className="bg-light p-0">
-                        <Table className="text-center table table-bordered" hover>
-                            <thead className="bg-primary">
+                    <Col className="bg-light p-0" style={{minHeight: 400}}>
+                        <Table className="text-center" hover responsive>
+                            <thead className="bg-primary text-white">
                             <tr className="text-white">
                                 <th>Nom de compagnie</th>
                                 <th>Nom complet de l'étudiant</th>
@@ -108,27 +76,35 @@ const SignerEntenteDeStageParGestionnaire = ({connectedUser}: { connectedUser: I
                             </tr>
                             </thead>
                             <tbody className="bg-light">
-                            {contratsNonSigner.map((contrat, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{contrat.companyName}</td>
-                                        <td>{contrat.studentFullName}</td>
-                                        <td>{contrat.position}</td>
-                                        <td>{contrat.session}</td>
-                                        <td>{contrat.description}</td>
-                                        <td>{contrat.companySignatureDate.split("T")[0]}</td>
-                                        <td>{contrat.studentSignatureDate.split("T")[0]}</td>
-                                        <td>{
-                                            contrat.gestionnaireSignature.length > 0 ?
-                                                <p>Déjà signé</p>
-                                                :
-                                                <Button className="btn btn-warning"
-                                                        onClick={async () => await getEntente(contrat.contractId)}>Signer</Button>
-
-                                        }</td>
-                                    </tr>
-                                );
-                            })}
+                            {contratsNonSigner.length === 0
+                                ? <tr>
+                                    <td colSpan={8}>
+                                        <p className="h1">Aucun contrat</p>
+                                    </td>
+                                </tr>
+                                : contratsNonSigner.map((contrat, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{contrat.companyName}</td>
+                                            <td>{contrat.studentFullName}</td>
+                                            <td>{contrat.position}</td>
+                                            <td>{contrat.session}</td>
+                                            <td>{contrat.description}</td>
+                                            <td>{contrat.companySignatureDate.split("T")[0]}</td>
+                                            <td>{contrat.studentSignatureDate.split("T")[0]}</td>
+                                            <td>
+                                                {contrat.gestionnaireSignature.length > 0 ?
+                                                    <p className="text-success">Vous avez signé</p> :
+                                                    <p className="text-danger">Vous n'avez pas signé!</p>}
+                                                <Button variant="success"
+                                                        disabled={contrat.gestionnaireSignature.length > 0}
+                                                        onClick={async () => await getEntente(contrat.contractId)}>
+                                                    Signer
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
                             </tbody>
                         </Table>
                     </Col>
